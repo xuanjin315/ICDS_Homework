f24703b59d37c407232542d97a76f3e90ee4c604
dexter <xuanjindong42@gmail.com>
2026-01-24 22:54:07 +0800

    first

---
+++ prime.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+    for i in range(2,int(n**0.5+1)):
+        if n%i==0:
+            return False
+        return True
+    
+
+if __name__ == "__main__":
+    
+    def list_prime(n):
+        p = []
+        for i in range(1, n):
+            if is_prime(i):
+                p.append(i)
+        return p
+
+    print(list_prime(10000))
